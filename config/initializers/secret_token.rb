2c33682357104cfdf686f5bce2b405172211d55a
Vincent Chang <vhchang@gmail.com>
2013-02-22 22:43:07 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wheelist::Application.config.secret_token = 'b3209f9776a74f478eb7cdaa14240ed89376346464e67b202849bf45e4eb260417a1a00c1eb1fce4203011fdc7496a9e20dcdfb562b3dbb2665f4020c1beefe0'
