1b2340c4c7cb14be5cfd5ff737d08d3fb9f79f18
Vincent Chang <vhchang@gmail.com>
2013-02-26 18:12:58 -0800

    Basic rendering of images after a search without a page redirect is now working.

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -35,14 +35,6 @@ class ImagesController < ApplicationController
 
 		@image = Image.new
 
-		# car_model_id = params[:image][:car_tags_attributes]["0"][:car_model_id]
-		# car_color_id = params[:image][:car_tags_attributes]["0"][:car_color_id]
-
-		# wheel_model_id  = params[:image][:wheel_tags_attributes]["0"][:wheel_model_id]
-		# wheel_size_id	  = params[:image][:wheel_tags_attributes]["0"][:wheel_size_id]
-		# wheel_offset_id	= params[:image][:wheel_tags_attributes]["0"][:wheel_offset_id]
-		# wheel_color_id	= params[:image][:wheel_tags_attributes]["0"][:wheel_color_id]
-
 		car_params   = params[:image][:car_tags_attributes]["0"]
 		wheel_params = params[:image][:wheel_tags_attributes]["0"]
 
@@ -54,36 +46,6 @@ class ImagesController < ApplicationController
 	  												wheel_color_id: wheel_params[:wheel_color_id])
 
 		@images = Image
-
-		# if car_model_id.present?
-		# 	@images = @images.joins(:car_tags)
-		# 								   .where(car_tags: { car_model_id: car_model_id })
-		# end
-
-		# if car_color_id.present?
-		# 	@images = @images.joins(:car_tags)
-		# 									 .where(car_tags: { car_color_id: car_color_id })
-		# end
-
-		# if wheel_model_id.present?
-		# 	@images = @images.joins(:wheel_tags)
-		# 									 .where(wheel_tags: { wheel_model_id: wheel_model_id })
-		# end
-
-		# if wheel_size_id.present?
-		# 	@images = @images.joins(:wheel_tags)
-		# 									 .where(wheel_tags: { wheel_size_id: wheel_size_id })
-		# end
-
-		# if wheel_offset_id.present?
-		# 	@images = @images.joins(:wheel_tags)
-		# 									 .where(wheel_tags: { wheel_offset_id: wheel_offset_id })
-		# end
-
-		# if wheel_color_id.present?
-		# 	@images = @images.joins(:wheel_tags)
-		# 									 .where(wheel_tags: { wheel_color_id: wheel_color_id })
-		# end
 
 		car_params.each do |key, value|
 			if value.present?
@@ -101,7 +63,13 @@ class ImagesController < ApplicationController
 
 		@images = @images.all
 
-		render 'index'
+
+		if request.xhr?				# if it's an AJAX request
+			render '_search', :layout => false
+		else
+			render 'index'
+		end
+
 	end
 
 	def like
