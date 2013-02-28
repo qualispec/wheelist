5d76589ff6a3907b3a50b0108ae1b055ed5118c6
Vincent Chang <vhchang@gmail.com>
2013-02-27 17:52:03 -0800

    Added 'paint_name' attribute to db/migrate/20130225214824_create_car_colors.rb. Corrected typo (capital L instead of lowercase l) in db/migrate/20130226064320_create_wheel_offset_options.rb.

--- db/migrate/20130225214824_create_car_colors.rb
+++ db/migrate/20130225214824_create_car_colors.rb
@@ -1,6 +1,7 @@
 class CreateCarColors < ActiveRecord::Migration
   def change
     create_table :car_colors do |t|
+      t.string :paint_name
     	t.string :color
     	t.integer :car_model_id
 
--- db/migrate/20130226064320_create_wheel_offset_options.rb
+++ db/migrate/20130226064320_create_wheel_offset_options.rb
@@ -2,7 +2,7 @@ class CreateWheelOffsetOptions < ActiveRecord::Migration
   def change
     create_table :wheel_offset_options do |t|
     	t.integer :wheel_model_id
-    	t.integer :wheeL_offset_id
+    	t.integer :wheel_offset_id
 
       t.timestamps
     end
