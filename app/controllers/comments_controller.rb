b6d49d8d87224c70161ec877f94f07e4245d8331
Vincent Chang <vhchang@gmail.com>
2013-03-01 18:47:45 -0800

    Comments can now be deleted. Added some more styling across the site. Ready to demo internally to class.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -15,7 +15,21 @@ class CommentsController < ApplicationController
     if request.xhr?       # if it's an AJAX request
       render '_update_comment', :layout => false
     else
-      redirect image_path(params[:comment][:image_id])
+      redirect_to image_path(params[:comment][:image_id])
+    end
+  end
+
+  def destroy
+    @comment = Comment.find(params[:id])
+    image_id = @comment.image.id
+
+    @comment.destroy
+
+    @comments = Comment.where(:image_id => image_id).all
+
+    if request.xhr?
+      render '_update_comment', :layout => false
+    else
     end
   end
 
