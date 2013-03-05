3cc15c48f13ce3a1bf255e555818c4b55508ecf6
Vincent Chang <vhchang@gmail.com>
2013-03-04 16:40:19 -0800

    Got Bootstrap modals working for sign up and sign in links in navbar.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,19 @@
 module ApplicationHelper
+
+  # The following 3 helper methods are to get the Devise signup/signin pages
+  # as Bootstrap modals.
+  # source: https://github.com/plataformatec/devise/wiki/How-To:-Display-a-custom-sign_in-form-anywhere-in-your-app
+
+  def resource_name
+    :user
+  end
+ 
+  def resource
+    @resource ||= User.new
+  end
+ 
+  def devise_mapping
+    @devise_mapping ||= Devise.mappings[:user]
+  end
+
 end
