e8b4acd960505f6971c915c329c8c705200c25ea
Vincent Chang <vhchang@gmail.com>
2013-08-14 20:50:31 -0700

    Minor code clean up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,7 +22,4 @@ class UsersController < ApplicationController
 		@user = User.find(params[:id])
 	end
 
-
-
-
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,7 +7,7 @@ module ApplicationHelper
   def resource_name
     :user
   end
- 
+
   def resource_class
     User
   end
@@ -15,7 +15,7 @@ module ApplicationHelper
   def resource
     @resource ||= User.new
   end
- 
+
   def devise_mapping
     @devise_mapping ||= Devise.mappings[:user]
   end
